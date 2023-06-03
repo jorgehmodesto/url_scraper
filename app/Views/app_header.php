5805d179bd8ae261931363a1ca9afbf4e1e0a53c
Jorge Henrique Modesto <jorgehmodesto@Jorges-MacBook-Pro.local>
2023-06-03 13:17:01 -0300

    feat: Adds some reusable content to rendering functionality

---
+++ app/Views/app_header.php
@@ -0,0 +1,173 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Koombea tech assessment</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+
+
+
+    <script type="text/javascript">
+
+        function scrape() {
+            $('#scrape_btn').fadeOut(0);
+            $('#loading_btn').fadeIn(0);
+        }
+
+        $(document).ready(function () {
+            $('#processed_pages').DataTable({
+                stripeClasses: ['striped'],
+            });
+        });
+
+    </script>
+
+    <!-- STYLES -->
+
+    <style>
+        * {
+            transition: background-color 300ms ease, color 300ms ease;
+        }
+        *:focus {
+            background-color: rgba(221, 72, 20, .2);
+            outline: none;
+        }
+        html, body {
+            color: rgba(33, 37, 41, 1);
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+            font-size: 16px;
+            margin: 0;
+            padding: 0;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-rendering: optimizeLegibility;
+        }
+        header {
+            background-color: rgba(247, 248, 249, 1);
+            padding: .4rem 0 0;
+        }
+        .menu {
+            padding: .4rem 2rem;
+        }
+        header ul {
+            list-style-type: none;
+            margin: 0;
+            overflow: hidden;
+            padding: 0;
+            text-align: right;
+        }
+        header li {
+            display: inline-block;
+        }
+        header li a {
+            box-sizing: unset!important;
+            border-radius: 5px;
+            color: rgba(0, 0, 0, .5);
+            display: block;
+            height: 44px;
+            text-decoration: none;
+        }
+        header li.menu-item a {
+            border-radius: 5px;
+            margin: 5px 0;
+            height: 38px;
+            line-height: 36px;
+            padding: .4rem .65rem;
+            text-align: center;
+        }
+        header li.menu-item a:hover,
+        header li.menu-item a:focus {
+            background-color: rgba(221, 72, 20, .2);
+            color: rgba(221, 72, 20, 1);
+        }
+
+        header li.menu-item a.selected {
+            background-color: rgba(221, 72, 20, .2);
+            color: rgba(221, 72, 20, 1);
+        }
+
+        header .logo {
+            float: left;
+            height: 44px;
+            padding: .4rem .5rem;
+        }
+
+        section.content {
+            max-width: 1024px;
+            margin: 20px auto;
+        }
+
+        .pages-table {
+            border-radius: 10px;
+            margin: 20px auto;
+            padding: 10px;
+            border: 3px #F0F0F0 solid;
+        }
+
+        .striped:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        #processed_pages td, #processed_pages th {
+            padding: 10px;
+        }
+
+        section {
+            margin: 0 auto;
+            max-width: 1100px;
+            padding: 2.5rem 1.75rem 3.5rem 1.75rem;
+        }
+
+        section h1 {
+            margin-bottom: 2.5rem;
+        }
+
+        section h2 {
+            font-size: 120%;
+            line-height: 2.5rem;
+            padding-top: 1.5rem;
+        }
+
+        section pre {
+            background-color: rgba(247, 248, 249, 1);
+            border: 1px solid rgba(242, 242, 242, 1);
+            display: block;
+            font-size: .9rem;
+            margin: 2rem 0;
+            padding: 1rem 1.5rem;
+            white-space: pre-wrap;
+            word-break: break-all;
+        }
+
+        section code {
+            display: block;
+        }
+
+        section svg {
+            margin-bottom: -5px;
+            margin-right: 5px;
+            width: 25px;
+        }
+
+        .further {
+            background-color: rgba(247, 248, 249, 1);
+            border-bottom: 1px solid rgba(242, 242, 242, 1);
+        }
+
+        .further h2:first-of-type {
+            padding-top: 0;
+        }
+
+        .hide {
+            display: none;
+        }
+    </style>
+</head>
